5c897bc78437a03728a07f4c5204ab2eeea18956
linkarys <linkarys@gmail.com>
2014-04-11 23:09:28 +0800

    add tree

--- app/scripts/filters/filter.js
+++ app/scripts/filters/filter.js
@@ -3,11 +3,11 @@
 angular.module('authApp')
 .filter('gender', function () {
 	return function (gender) {
-		return gender == 1 ? "男" : "女"
-	};
+		return gender == 1 ? "男" : "女";
+	}
 })
 .filter('valid', function () {
 	return function (valid) {
-		return valid == 1 ? "有效" : "无效"
-	};
+		return valid == 1 ? "有效" : "无效";
+	}
 })
