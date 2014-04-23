6db63dce0ffd4097c2f140c623cb425006259ae8
linkarys <linkarys@gmail.com>
2014-04-23 20:03:33 +0800

    update user info and privilege filter

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,13 @@
 'use strict';
 
 angular.module('mainApp')
-.controller('MainCtrl', ['$scope', '$state', '$stateParams', 'BasicService', '$http', 'md5', function ($scope, $state, $stateParams, BasicService, $http, md5) {
+.controller('MainCtrl', ['$scope', '$state', '$stateParams', 'Auth', '$http', 'md5', function ($scope, $state, $stateParams, Auth, $http, md5) {
 
 	$scope.$state = $state;
 	$scope.$stateParams = $stateParams;
+
+	$scope.date = new Date();
+	$scope.currentUser = Auth.getCurrentUser();
+
+	console.log($scope.currentUser);
 }])
--- app/scripts/services/authservice.js
+++ app/scripts/services/authservice.js
@@ -1,6 +1,7 @@
 'use strict';
-angular.module('mainApp').factory('Auth', function($http, ipCookie, $location, PostService) {
-	var accessLevels = [];
+angular.module('mainApp').service('Auth', function($http, ipCookie, $location, PostService) {
+	var accessLevels = [],
+		currentUser;
 
 	function updateUser(user) {
 		ipCookie.remove('uuduser');
@@ -17,87 +18,89 @@ angular.module('mainApp').factory('Auth', function($http, ipCookie, $location, P
 		return str.join("&");
 	}
 
-	return {
-		authorize: function(accessCode) {
-			if (typeof accessCode === 'undefined') {return true;}
+	this.getCurrentUser = function() {
+		if (!currentUser) {
+			currentUser = ipCookie('uuduser').user;
+		}
 
-			for (var i = accessLevels.length - 1; i >= 0; i--) {
-				if (accessLevels[i].code == accessCode) {
-					return true;
-				}
-			};
-			return false;
-		},
-		loadAccessLevels: function() {
-			PostService.get(config.auth.baseurl + config.auth.resource)
-				.success(function(data) {
-					accessLevels = data;
-				})
-				.error(function() {
-					accessLevels = [
-						{code: '10'},
-						{code: '4'},
-						{code: '12'}
-					]
-				})
-		},
-		getAccessLevels: function() {
-			return accessLevels;
-		},
-		isLoggedIn: function(user) {
-			var user = ipCookie('uuduser');
+		return currentUser;
+	}
 
-			if (user && user.token) {
-				// updateUser(user);
-				console.log('user is:');
-				console.log(user);
-				console.log('token is:');
-				console.log(user.token);
+	this.authorize = function(accessCode) {
+		if (typeof accessCode === 'undefined') {return true;}
+
+		for (var i = accessLevels.length - 1; i >= 0; i--) {
+			if (accessLevels[i].code == accessCode) {
 				return true;
 			}
+		};
+		return false;
+	}
 
-			return false;
-		},
+	this.loadAccessLevels = function() {
+		PostService.get(config.auth.baseurl + config.auth.resource)
+			.success(function(data) {
+				accessLevels = data;
+			})
+	}
 
-		login: function(user, success, error) {
+	this.getAccessLevels = function() {
+		return accessLevels;
+	}
+
+	this.isLoggedIn = function(user) {
+		var user = ipCookie('uuduser');
+
+		if (user && user.token) {
+			// updateUser(user);
+			console.log('user is:');
+			console.log(user);
+			console.log('token is:');
+			console.log(user.token);
+			return true;
+		}
+
+		return false;
+	}
+
+	this.login = function(user, success, error) {
 
 
-			$http.get(config.auth.baseurl + config.auth.login + '?' + serialize(user))
-				.success(function(res) {
-					// 登录成功
-					if (res.legal) {
-						success('0');
-						updateUser(res);
-						$location.path('/');
-					} else {
-						// 用户名或密码错误
-						var errorCode = res.message.split(':')[0];
-						success(errorCode);
-					}
-				})
-					.error(function() {
-						if (angular.isFunction(error)) {
-							error();
-						}
-					});
+		$http.get(config.auth.baseurl + config.auth.login + '?' + serialize(user))
+			.success(function(res) {
+				// 登录成功
+				if (res.legal) {
+					success('0');
+					updateUser(res);
+					$location.path('/');
+				} else {
+					// 用户名或密码错误
+					var errorCode = res.message.split(':')[0];
+					success(errorCode);
+				}
+			})
 
-			var user = {
+		var res = {
+			token: 'myuudtoken-hahahhahhahah',
+			user: {
 				name: 'adsfasdf',
-				asdfa: 'asdfasdf',
-				token: 'myuudtoken-hahahhahhahah'
+				phone: '123123',
+				positions: 'admin',
+				userNo: '09'
 			}
-			updateUser(user);
-			$location.path('/');
-		},
-		logout: function(success, error) {
+		}
+		updateUser(res);
+		$location.path('/');
+	}
+
+	this.logout = function(success, error) {
 /*			$http.get('/at/ws/auth/logout').success(function() {
-				ipCookie.remove('uuduser');
-				$location.path('/login');
-				success();
-			}).error(error);*/
-			console.log('log out');
 			ipCookie.remove('uuduser');
 			$location.path('/login');
-		}
-	};
+			success();
+		}).error(error);*/
+		console.log('log out');
+		ipCookie.remove('uuduser');
+		$location.path('/login');
+	}
 });
