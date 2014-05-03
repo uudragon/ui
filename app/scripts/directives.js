e10ce1250d5932a5a8f0facc7cdd98c6ebcab891
linkarys <linkarys@gmail.com>
2014-05-03 23:26:47 +0800

    add line holder

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -523,7 +523,6 @@ uud.directive('timing', ['$interval', 'dateFilter',
 .directive('uuPieChart', function() {
 	return {
 		restrict: 'A',
-		require: 'ngModel',
 		link: function($scope, elem, attrs, ctrl) {
 			var w = 300;
 			var h = 300;
@@ -852,6 +851,86 @@ uud.directive('timing', ['$interval', 'dateFilter',
 					return "translate(" + Math.cos(val) * (r+textOffset) + "," + Math.sin(val) * (r+textOffset) + ")";
 				};
 			}
+		}
+	}
+})
+
+.directive('uuLineChart', function() {
+	return {
+		scope: {
+			data: '='
+		},
+		link: function($scope, elem, attrs) {
+			var data = $scope.data,
+			w = 320,
+			h = 200,
+			margin = 20,
+			y = d3.scale.linear().domain([0, d3.max(data)]).range([0 + margin, h - margin]),
+			x = d3.scale.linear().domain([0, data.length]).range([0 + margin, w - margin]);
+			var vis = d3.select(elem[0])
+			.append("svg:svg")
+			.attr("width", w)
+			.attr("height", h);
+			 
+			var g = vis.append("svg:g")
+			.attr("transform", "translate(0, 200)");
+
+			var line = d3.svg.line()
+			.x(function(d,i) { return x(i); })
+			.y(function(d) { return -1 * y(d); });
+
+			g.append("svg:path")
+			.attr("class", "line-path")
+			.attr("d", line(data));
+
+			g.append("svg:line")
+			.attr("x1", x(0))
+			.attr("y1", -1 * y(0))
+			.attr("x2", x(w))
+			.attr("y2", -1 * y(0))
+
+			g.append("svg:line")
+			.attr("x1", x(0))
+			.attr("y1", -1 * y(0))
+			.attr("x2", x(0))
+			.attr("y2", -1 * y(d3.max(data)))
+
+			g.selectAll(".xLabel")
+			.data(x.ticks(5))
+			.enter().append("svg:text")
+			.attr("class", "xLabel")
+			.text(String)
+			.attr("x", function(d) { return x(d) })
+			.attr("y", 0)
+			.attr("text-anchor", "middle")
+
+			g.selectAll(".yLabel")
+			.data(y.ticks(4))
+			.enter().append("svg:text")
+			.attr("class", "yLabel")
+			.text(String)
+			.attr("x", 0)
+			.attr("y", function(d) { return -1 * y(d) })
+			.attr("text-anchor", "right")
+			.attr("dy", 4)
+
+			g.selectAll(".xTicks")
+			.data(x.ticks(5))
+			.enter().append("svg:line")
+			.attr("class", "xTicks")
+			.attr("x1", function(d) { return x(d); })
+			.attr("y1", -1 * y(0))
+			.attr("x2", function(d) { return x(d); })
+			.attr("y2", -1 * y(-0.3))
+			 
+			g.selectAll(".yTicks")
+			.data(y.ticks(4))
+			.enter().append("svg:line")
+			.attr("class", "yTicks")
+			.attr("y1", function(d) { return -1 * y(d); })
+			.attr("x1", x(-0.3))
+			.attr("y2", function(d) { return -1 * y(d); })
+			.attr("x2", x(0))
 		}
 	}
 })
