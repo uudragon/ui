8efacefbb34fc093dc32bb71e1961739e09dd68e
linkarys <linkarys@gmail.com>
2015-01-07 11:03:42 +0800

    tweak

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,23 +9,24 @@ function ($scope, $state, $stateParams, Auth, Resource, $filter, $http) {
 	$scope.date = new Date();
 
 	// for debug
-	$scope.currentUser = Auth.getUser() || {
-		account: 'admin',
-		birthday: '2014-04-02 00:00',
-		email: '1',
-		extension: null,
-		gender: 1,
-		groupId: null,
-		id: 1,
-		isRemoved: false,
-		isValid: true,
-		name: 'admin',
-		phone: '1',
-		positions: '1',
-		roleId: 234234,
-		seat: null,
-		userNo: '000010'
-	};
+	$scope.currentUser = Auth.getUser();
+	// $scope.currentUser = Auth.getUser() || {
+	// 	account: 'admin',
+	// 	birthday: '2014-04-02 00:00',
+	// 	email: '1',
+	// 	extension: null,
+	// 	gender: 1,
+	// 	groupId: null,
+	// 	id: 1,
+	// 	isRemoved: false,
+	// 	isValid: true,
+	// 	name: 'admin',
+	// 	phone: '1',
+	// 	positions: '1',
+	// 	roleId: 234234,
+	// 	seat: null,
+	// 	userNo: '000010'
+	// };
 
 	$scope.searchModel = {
 		filter: 0,
